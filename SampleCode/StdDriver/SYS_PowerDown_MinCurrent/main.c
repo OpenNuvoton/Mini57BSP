c810a8d3c36af641154595bf414883bad720a31f
cjchen1 <cjchen1@nuvoton.com>
2023-04-07 11:42:11 +0800

    BSP: Add new sample code SYS_PowerDown_MinCurrent.
    Change-Id: Iefef925e6781355558f5ac61aaf041becdf19565

---
+++ SampleCode/StdDriver/SYS_PowerDown_MinCurrent/main.c
@@ -0,0 +1,157 @@
+/******************************************************************************
+ * @file     main.c
+ * @version  V1.00
+ * $Revision: 1 $
+ * $Date: 17/04/19 7:49p $
+ * @brief    Demonstrate how to minimize power consumption when entering power down mode.
+ * @note
+ * SPDX-License-Identifier: Apache-2.0
+ * @copyright (C) 2020 Nuvoton Technology Corp. All rights reserved.
+*****************************************************************************/
+#include <stdio.h>
+#include "Mini57Series.h"
+
+#define GPIO_P0_TO_P15              0xFFFF
+
+/*----------------------------------------------------------*/
+/*  Function for disable internal analog POR circuit        */
+/*----------------------------------------------------------*/
+void SYS_Disable_AnalogPORCircuit(void)
+{
+    SYS_DISABLE_POR();
+}
+
+/*----------------------------------------------------------*/
+/*  Function for System Entry to Power Down Mode            */
+/*----------------------------------------------------------*/
+void PowerDownFunction(void)
+{
+    /* Check if all the debug messages are finished */
+    UUART_WAIT_TX_EMPTY(UUART0);
+
+    /* Enter to Power-down mode */
+    CLK_PowerDown();
+}
+
+void GPABCD_IRQHandler(void)
+{
+    volatile uint32_t temp;
+
+    /* To check if PB.3 interrupt occurred */
+    if(GPIO_GET_INT_FLAG(PB, BIT3))
+    {
+        GPIO_CLR_INT_FLAG(PB, BIT3);
+        printf("PB.3 INT occurred.\n");
+    }
+    else
+    {
+        /* Un-expected interrupt. Just clear all PB interrupts */
+        temp = PB->INTSRC;
+        PB->INTSRC = temp;
+        printf("Un-expected interrupts.\n");
+    }
+}
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Enable 48MHz HIRC */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRC_EN);
+
+    /* Waiting for 48MHz clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+    /* HCLK Clock source from HIRC */
+    CLK_SetHCLK(CLK_HCLK_SRC_HIRC, CLK_CLKDIV_HCLK(1));
+
+    /* Enable USCI0 IP clock */
+    CLK_EnableModuleClock(USCI0_MODULE);
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock and cyclesPerUs automatically. */
+    SystemCoreClockUpdate();
+
+    /* USCI-Uart0-GPD5(TX) + GPD6(RX) */
+    /* Set GPD multi-function pins for USCI UART0 GPD5(TX) and GPD6(RX) */
+    SYS->GPD_MFP = (SYS->GPD_MFP & ~(SYS_GPD_MFP_PD5MFP_Msk | SYS_GPD_MFP_PD6MFP_Msk)) |
+                   (SYS_GPD_MFP_PD5_UART0_TXD | SYS_GPD_MFP_PD6_UART0_RXD);
+
+    /* Set GPD5 as output mode and GPD6 as Input mode */
+    GPIO_SetMode(PD, BIT5, GPIO_MODE_OUTPUT);
+    GPIO_SetMode(PD, BIT6, GPIO_MODE_INPUT);
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+int main()
+{
+    SYS_Init();
+
+    /* Init USCI UART0 to 115200-8n1 for print message */
+    UUART_Open(UUART0, 115200);
+
+    printf("\n\nPDID 0x%08X\n", SYS_ReadPDID());    /* Display PDID */
+    printf("CPU @ %dHz\n", SystemCoreClock);        /* Display System Core Clock */
+
+    printf("+-------------------------------------------------------------------+\n");
+    printf("| Mini57 SYS_PowerDown_MinCurrent and Wake-up by PB.3 Sample Code   |\n");
+    printf("+-------------------------------------------------------------------+\n");
+
+    printf("+-------------------------------------------------------------------------+\n");
+    printf("| Operating sequence                                                      |\n");
+    printf("|  1. Remove all continuous load, e.g. LED.                               |\n");
+    printf("|  2. Configure all GPIO as Quasi-bidirectional Mode                      |\n");
+    printf("|  3. Must disable LVR                                                    |\n");
+    printf("|  4. Disable analog function, e.g. ADC, ACMP, and POR module.            |\n");
+    printf("|  5. Enter to Power-Down                                                 |\n");
+    printf("|  6. Wait for PB.3 falling-edge interrupt event to wakeup the MCU        |\n");
+    printf("+-------------------------------------------------------------------------+\n\n");
+
+    /* Check if all the debug messages are finished */
+    UUART_WAIT_TX_EMPTY(UUART0);
+
+    /* Configure all GPIO as Quasi-bidirectional Mode*/
+    GPIO_SetMode(PA, GPIO_P0_TO_P15, GPIO_MODE_QUASI);
+    GPIO_SetMode(PB, GPIO_P0_TO_P15, GPIO_MODE_QUASI);
+    GPIO_SetMode(PC, GPIO_P0_TO_P15, GPIO_MODE_QUASI);
+    GPIO_SetMode(PD, GPIO_P0_TO_P15, GPIO_MODE_QUASI);
+
+    /* Configure PB.3 as Quasi-bidirectional mode and enable interrupt by falling edge trigger */
+    GPIO_SetMode(PB, BIT3, GPIO_MODE_QUASI);
+    GPIO_EnableInt(PB, 3, GPIO_INT_FALLING);
+    NVIC_EnableIRQ(GP_IRQn);
+
+    /* Enable interrupt de-bounce function and select de-bounce sampling cycle time is 1024 clocks of LIRC clock */
+    GPIO_SET_DEBOUNCE_TIME(GPIO_DBCTL_DBCLKSRC_LIRC, GPIO_DBCTL_DBCLKSEL_1024);
+    GPIO_ENABLE_DEBOUNCE(PB, BIT3);
+
+    /* Unlock protected registers before entering Power-down mode */
+    SYS_UnlockReg();
+
+    /* LIRC must be disabled */
+    CLK_DisableXtalRC(CLK_PWRCTL_LIRC_EN);
+
+    /* LVR must be disabled */
+    SYS_DISABLE_LVR();
+
+    /* BOD must be disabled */
+    SYS_DISABLE_BOD();
+
+    /* Turn off internal analog POR circuit */
+    SYS_Disable_AnalogPORCircuit();
+    /* Disable Power-on Reset */
+    SYS_DISABLE_POR();
+
+    printf("Enter to Power-Down ......\n");
+    /* Enter to Power-down mode */
+    PowerDownFunction();
+    /* Waiting for PB.3 falling-edge interrupt event */
+    printf("System waken-up done.\n\n");
+
+    while(1);
+}
+
+/*** (C) COPYRIGHT 2020 Nuvoton Technology Corp. ***/
